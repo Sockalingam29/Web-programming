c7e334f8576b3df24e005cff7c9f5794ddcb714f
HARIMUTHU-M <harimuthutce@gmail.com>
2023-02-12 22:15:16 +0530

    Co-authored-by: Sockalingam <Sockalingam29@users.noreply.github.com>

--- home.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll('.product-item');
-items.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        item.style.cursor = 'pointer';
-    });
-    }
-);
