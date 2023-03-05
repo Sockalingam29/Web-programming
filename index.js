8b0841c75de51116d3a3f44f52ce5660962a91f7
HARIMUTHU-M <harimuthutce@gmail.com>
2023-03-05 23:40:41 +0530

    cart file

--- index.js
+++ index.js
@@ -5,6 +5,63 @@ items.forEach((item) => {
   });
 });
 
+// $(document).ready(function() {
+//   $(document).on('submit', '#search-form', function() {
+//     // do your things
+//     return false;
+//    });
+// });
+
+// $("#search-form").submit(function (e) {
+//   console.log("Hello");
+//   e.preventDefault(); // prevent form submission and page reload
+//   var searchQuery = $("input[name='searchQuery']").val();
+//   $.ajax({
+//     url: "SearchProductsServlet",
+//     data: {
+//       searchQuery: searchQuery,
+//     },
+//     success: function (response) {
+//       // process the response here
+//       // update the DOM to display the matching products
+//       var productsList = $("<ul>");
+//       $.each(response, function (index, value) {
+//         var product = $("<li>").text(value.name + " - " + value.price);
+//         productsList.append(product);
+//       });
+//       $("#search-results").html(productsList);
+//     },
+//   });
+//   return false;
+// });
+
+// document
+//   .getElementById("search-form")
+//   .addEventListener("submit",
+
+function submitHandler(event) {
+  console.log("Hello");
+  event.preventDefault(); // prevent form submission and page reload
+  var searchQuery = document.querySelector("input[name='searchQuery']").value;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // process the response here
+      var response = JSON.parse(xhr.responseText);
+      var productsList = document.createElement("ul");
+      response.forEach(function (value) {
+        var product = document.createElement("li");
+        product.textContent = value.name + " - " + value.price;
+        productsList.appendChild(product);
+      });
+      document.getElementById("search-results").appendChild(productsList);
+    }
+  };
+  xhr.open("GET", "SearchProductsServlet?searchQuery=" + searchQuery);
+  xhr.send();
+}
+
+
 $(document).ready(function () {
   $("#dark-mode-icon").click(function () {
     $("body").toggleClass("dark-mode");
