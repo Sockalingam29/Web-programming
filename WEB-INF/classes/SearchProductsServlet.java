f64f97e5b4fc418eae62e56851903d2b3d9967c2
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2023-03-06 12:13:48 +0530

    Update SearchProductsServlet.java

--- WEB-INF/classes/SearchProductsServlet.java
+++ WEB-INF/classes/SearchProductsServlet.java
@@ -1,84 +1,68 @@
-// importing the javax.servlet package
-// importing java.io package for PrintWriter
-import javax.servlet.*;
+import jakarta.servlet.*;
 import java.io.*;
 import java.util.*;
-import javax.servlet.http.*;
+import jakarta.servlet.http.*;
 import com.google.gson.Gson;
 
 
-// Extend HttpServlet class
 public class SearchProductsServlet extends HttpServlet {
 
 
-String[] img = {
-    "https://www.aptronixindia.com/media/catalog/product/cache/31f0162e6f7d821d2237f39577122a8a/m/b/mbp14-spacegray-select-202110-removebg-preview.png",
-    "https://www.pngall.com/wp-content/uploads/13/Nothing-Phone-1-PNG-Cutout.png",
-    "https://educatehill.com/wp-content/uploads/2023/01/image-114.png",
-    "https://lapgadgets.in/wp-content/uploads/2019/08/green-ssd-sata-family.png.thumb_.1280.1280-1024x1024.png",
-    "https://fundamental.in/wp-content/uploads/2021/06/234516_1_wc7roq.png",
-    "https://image01.realme.net/general/20220711/1657521687391.png"
-};
-    
-    
+    String[] img = {
+        "https://www.aptronixindia.com/media/catalog/product/cache/31f0162e6f7d821d2237f39577122a8a/m/b/mbp14-spacegray-select-202110-removebg-preview.png",
+        "https://www.pngall.com/wp-content/uploads/13/Nothing-Phone-1-PNG-Cutout.png",
+        "https://educatehill.com/wp-content/uploads/2023/01/image-114.png",
+        "https://lapgadgets.in/wp-content/uploads/2019/08/green-ssd-sata-family.png.thumb_.1280.1280-1024x1024.png",
+        "https://fundamental.in/wp-content/uploads/2021/06/234516_1_wc7roq.png",
+        "https://image01.realme.net/general/20220711/1657521687391.png"
+    };
 
-String[] name = {
-"Apple M1 Mac",
-"Nothing Phone (1)",
-"Samsung S22 Ultra",
-"WD SSD combo",
-"Sony Bravia 40\" LED Tv",
-"Realme watch 3"
-};
+    String[] name = {
+        "Apple M1 Mac",
+        "Nothing Phone (1)",
+        "Samsung S22 Ultra",
+        "WD SSD combo",
+        "Sony Bravia 40\" LED Tv",
+        "Realme watch 3"
+    };
 
-
-String [] price = {
-"Rs 91,999",
-"Rs 31,999",
-"Rs 84,999",
-"Rs 6,999",
-"Rs 49,999",
-"Rs 3,999"
-};
+    String [] price = {
+        "Rs 91,999",
+        "Rs 31,999",
+        "Rs 84,999",
+        "Rs 6,999",
+        "Rs 49,999",
+        "Rs 3,999"
+    };
 
 
-private List<String> searchProducts(String searchQuery) {
-    List<String> products = new ArrayList<>();
-    // perform database search for products based on search query
-    // add matching products to the products list
-    for (int i = 0; i < name.length; i++) {
-        if (name[i].toLowerCase().contains(searchQuery.toLowerCase())) {
-            products.add(name[i]);
+    private List<String> searchProducts(String searchQuery) {
+        List<String> products = new ArrayList<>();
+        for (int i = 0; i < name.length; i++) {
+            if (name[i].toLowerCase().contains(searchQuery.toLowerCase())) {
+                products.add(name[i]);
+            }
         }
+        return products;
+    }    
+
+    public void init() throws ServletException {
     }
-    return products;
-}    
-public void init() throws ServletException {
-// Do required initialization
 
-}
 
-// public void doGet(HttpServletRequest request,
-// HttpServletResponse response)
-// throws ServletException, IOException {
-// // Set response content type
-// response.setContentType("text/html");
-// // Actual logic goes here.
-// PrintWriter out = response.getWriter();
-// out.println("<h1>" + "Hello" + "</h1>");
-// }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String searchQuery = request.getParameter("searchQuery");
+        List<String> products = searchProducts(searchQuery);
+        
+        Gson gson = new Gson();
 
-public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String searchQuery = request.getParameter("searchQuery");
-    List<String> products = searchProducts(searchQuery);
-    Gson gson = new Gson();
+        String json = gson.toJson(products);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
 
-    String json = gson.toJson(products);
-    response.setContentType("application/json");
-    response.setCharacterEncoding("UTF-8");
-    response.getWriter().write(json);
-}
-public void destroy() {
-// do nothing.
-}
+    public void destroy() {
+    }
 }
